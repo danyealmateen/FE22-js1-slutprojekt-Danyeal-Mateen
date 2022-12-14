bca6032fd48b3ca88ded8a7b7720c386c0c9c112
danyealmateen <danyeal.mateen@hotmail.com>
2022-12-14 12:51:47 +0100

    added buttons

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ let button = document.getElementById("button");
 button.addEventListener("click", (e) => {
   e.preventDefault();
   renderFlickrPic();
-  console.log("Knappen klickad");
 });
 
 function renderFlickrPic() {
-  let input = document.getElementById("input").value;
+  let inputText = document.getElementById("inputText").value;
   let inputNumber = document.getElementById("inputNumber").value;
-  console.log("Funktionen körs");
-  console.log(input);
+  let inputDateMin = document.getElementById("inputDateMin").value;
+  let inputDateMax = document.getElementById("inputDateMax").value;
+  let url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=53e8d7af0f521f892d0883605d2e7213&text=${inputText}&per_page=${inputNumber}&min_upload_date=${inputDateMin}&max_upload_date=${inputDateMax}&format=json&nojsoncallback=1 `;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+  console.log(inputText);
+  console.log(inputNumber);
+  console.log(inputDateMin);
+  console.log(inputDateMax);
 }
