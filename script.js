5a734b71c5bc9d94480abd71ff16e22b8eef7d6d
danyealmateen <danyeal.mateen@hotmail.com>
2022-12-14 16:01:48 +0100

    asd

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let divForThePictures = document.getElementById("divForThePictures");
+let animeParagraph = document.getElementById("animeParagraph");
 let button = document.getElementById("button");
 
 button.addEventListener("click", (e) => {
   clearTheDivForPictures();
   e.preventDefault();
   renderFlickrPic();
+  anime();
 });
 
 function renderFlickrPic() {
@@ -24,17 +26,36 @@ function renderFlickrPic() {
     checkboxValue = "z";
   } else if (checkboxLarge.checked) {
     checkboxValue = "b";
+  } else {
+    let checkboxError = document.createElement("h1");
+    checkboxError.innerHTML = `<li>Please choose a img size!</li>`;
+    document.body.appendChild(checkboxError);
   }
 
   if (inputSorting === "Date posted") {
     inputSortingValue = "date-posted-asc";
+    console.log(inputSortingValue);
   } else if (inputSorting === "Interestingness") {
     inputSortingValue = "interestingness-asc";
+    console.log(inputSortingValue);
   } else if (inputSorting === "Relevance") {
     inputSortingValue = "relevance";
+    console.log(inputSortingValue);
   }
 
-  let url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=53e8d7af0f521f892d0883605d2e7213&text=${inputText}&per_page=${inputNumber}&sort=${inputSorting}&format=json&nojsoncallback=1 `;
+  const animation = anime({
+    targets: "p",
+    color: "white",
+    translateX: 5,
+    translateY: 5,
+    backgroundColor: "hsl(330, 100%, 71%)",
+    border: "dotted 10px orange",
+    duration: 400,
+    easing: "linear",
+    direction: "alternate",
+  });
+
+  let url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=53e8d7af0f521f892d0883605d2e7213&text=${inputText}&per_page=${inputNumber}&sort=${inputSortingValue}&format=json&nojsoncallback=1 `;
 
   fetch(url)
     .then((response) => response.json())
@@ -42,7 +63,12 @@ function renderFlickrPic() {
       data.photos.photo.forEach((obj) => {
         divForThePictures.innerHTML += `<img src="https://live.staticflickr.com/${obj.server}/${obj.id}_${obj.secret}_${checkboxValue}.jpg"/>`;
       });
-      console.log(data.photos.photo);
+    })
+    .catch((error) => {
+      let createErrorMessage = document.createElement("h1");
+      createErrorMessage.innerHTML = `<li>Please fill in the "Search..." input!</li>`;
+      document.body.appendChild(createErrorMessage);
+      console.log(error);
     });
 }
 
