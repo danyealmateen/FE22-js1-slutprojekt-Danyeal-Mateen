93be874175bbab7cc61d06d2aa137517b06a37c1
dany <danyeal.mateen@hotmail.com>
2022-12-18 21:49:36 +0100

    uppdaterad

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let divForThePictures = document.getElementById("divForThePictures");
 let animeParagraph = document.getElementById("animeParagraph");
 let button = document.getElementById("button");
+let randomButton = document.getElementById("randomButton");
+let errorContainer = document.getElementById("error-message-container");
 
 //Ett event som lyssnar på ett knapptryck där den först:
 //1. Rensar divven som har img bilderna i sig,
@@ -9,9 +11,19 @@ let button = document.getElementById("button");
 //3. Skjuter iväg funktionen renderFlickrPic,
 //4. Skjuter iväg funktionen för animeringen
 button.addEventListener("click", (e) => {
+  errorContainer.innerHTML = "";
   clearTheDivForPictures();
   e.preventDefault();
   renderFlickrPic();
+  anime();
+});
+
+//Samma som ovan.
+randomButton.addEventListener("click", (e) => {
+  errorContainer.innerHTML = "";
+  clearTheDivForPictures();
+  e.preventDefault();
+  renderRandomFlickrPics();
   anime();
 });
 
@@ -21,6 +33,9 @@ function renderFlickrPic() {
   let inputNumber = document.getElementById("inputNumber").value;
   let inputSorting = document.getElementById("inputSorting").value;
   let inputSortingValue = "";
+
+  //Kollar om error är falsk för returnen längre ner.
+  let isError = false;
 
   //Samtliga checkboxfält
   let checkboxSmall = document.getElementById("checkboxSmall");
@@ -36,9 +51,18 @@ function renderFlickrPic() {
   } else if (checkboxLarge.checked) {
     checkboxValue = "b";
   } else {
-    let checkboxError = document.createElement("h1");
-    checkboxError.innerHTML = `<li>Please choose a img size!</li>`;
-    document.body.appendChild(checkboxError);
+    let createCheckboxErrorMessage = document.createElement("h1");
+    createCheckboxErrorMessage.innerHTML = `<li>Please choose a img size!</li>`;
+    errorContainer.appendChild(createCheckboxErrorMessage);
+    isError = true;
+  }
+
+  //Hanterar errormeddelanden
+  if (inputText === "") {
+    let createErrorMessage = document.createElement("h1");
+    createErrorMessage.innerHTML = `<li>Please fill in the "Search..." input!</li>`;
+    errorContainer.appendChild(createErrorMessage);
+    isError = true;
   }
 
   //If sats som konceptmässigt gör samma sak som ovan fast med sorteringsfälten.
@@ -55,7 +79,7 @@ function renderFlickrPic() {
 
   //Animationen som kallas när man klickar på knappen.
   const animation = anime({
-    targets: "p",
+    targets: "#animeDiv",
     color: "white",
     translateX: 5,
     translateY: 5,
@@ -68,7 +92,10 @@ function renderFlickrPic() {
 
   //Hämtar APIet från FLICKR med min egen personliga nyckel som jag skapat på hemsidan. Har ersatt attributen med värden från mina egna inputfält så att användaren själv kan välja vad som ska visas och hur.
   let url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=53e8d7af0f521f892d0883605d2e7213&text=${inputText}&per_page=${inputNumber}&sort=${inputSortingValue}&format=json&nojsoncallback=1 `;
-
+  //Om isError är true så ska den inte fetcha någonting utan avsluta skriptet.
+  if (isError) {
+    return;
+  }
   //Hämtar URL:et
   fetch(url)
     //Konverterar det till json objekt.
@@ -76,35 +103,28 @@ function renderFlickrPic() {
     .then((data) => {
       //Loopar igenom arrayen så att jag kan hämta de olika objekten till mitt img element som jag skapar i min div (divForThePictures). Precis som ovan så ersätter jag attributen i URL fast denna gången med data från objektet som jag brytit ner för att kunna rendera bilderna jag hämtar in.
       data.photos.photo.forEach((obj) => {
-        divForThePictures.innerHTML += `<img src="https://live.staticflickr.com/${obj.server}/${obj.id}_${obj.secret}_${checkboxValue}.jpg"/>`;
+        divForThePictures.innerHTML += `<img id="images" src="https://live.staticflickr.com/${obj.server}/${obj.id}_${obj.secret}_${checkboxValue}.jpg"/>`;
       });
+      console.log(data);
     })
     //Fångar upp error meddelanden och loggar det i konsolen samt ett meddelande skrivs ut till DOM:en.
     .catch((error) => {
-      let createErrorMessage = document.createElement("h1");
-      createErrorMessage.innerHTML = `<li>Please fill in the "Search..." input!</li>`;
-      document.body.appendChild(createErrorMessage);
       console.log(error);
     });
 }
-
 //Rensar innehållet av divven.
 function clearTheDivForPictures() {
   divForThePictures.innerHTML = "";
 }
-
 //!!EXTRA funktionalitet!! Gjort en counter som räknar från 0 till 60.000.000 med en random fakta till användaren där den berättar hur många aktiva användare FLICKR har.
 const counters = document.querySelectorAll(".counter");
 
 counters.forEach((counter) => {
   counter.innerText = "0";
-
   const updateCounter = () => {
     const target = +counter.getAttribute("data-target");
     const c = +counter.innerText;
-
     const increment = target / 200;
-
     if (c < target) {
       counter.innerText = `${Math.ceil(c + increment)}`;
       setTimeout(updateCounter, 1);
@@ -112,6 +132,23 @@ counters.forEach((counter) => {
       counter.innerText = target;
     }
   };
-
   updateCounter();
 });
+
+//!!!EXTRA Funktionalitet!!! Hämtar API:et men med olika attribut och är kopplad till randomButton knappen. Denna funktionen generarar 10 random bilder med hjälp av en Math Random variabel.
+function renderRandomFlickrPics() {
+  let rand = Math.floor(Math.random() * 10000);
+  let url = `https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=53e8d7af0f521f892d0883605d2e7213&per_page=10&sort=date-posted-desc&page=${rand}&format=json&nojsoncallback=1 `;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      data.photos.photo.forEach((obj) => {
+        divForThePictures.innerHTML += `<img id="images" src="https://live.staticflickr.com/${obj.server}/${obj.id}_${obj.secret}_m.jpg"/>`;
+      });
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
